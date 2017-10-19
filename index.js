07a51966913b3028bc3eae78b562ce14d2cd6cac
RJ Sanz <jykp_er@hotmail.com>
2017-10-19 16:35:49 -0500

    Commit ALV

---
+++ index.js
@@ -0,0 +1,18 @@
+// function hola(){
+//   return 'Hola mundo'
+// }
+
+const lodash = require('lodash')
+
+const obj = {
+  nombre: 'Raul',
+  edad: '20',
+  email: 'jykp_er@hotmail.com'
+}
+console.log(obj.email);
+
+// console.log(hola())
+// console.log(2+2)
+//
+// var lista =[1,2,3,4,5]
+// cosole.console.log(lista);
